6f84aff4f4dca62456cf2a9410cecf776310f4aa
danielmegale <danim1@live.it>
2023-06-29 14:54:49 +0200

    added data

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,8 +1,71 @@
 <script>
-    export default{}
+    export default{
+        data(){
+            return{
+                links:[
+                    {
+                        text: 'Characters',
+                        url: '#',
+                        current: false,
+                    },
+                    {
+                        text: 'Comics',
+                        url: '#',
+                        current: true,
+                    },
+                    {
+                        text: 'Movies',
+                        url: '#',
+                        current: false,
+                    },
+                    {
+                        text: 'TV',
+                        url: '#',
+                        current: false,
+                    },
+                    {
+                        text: 'Games',
+                        url: '#',
+                        current: false,
+                    },
+                    {
+                        text: 'Collectibles',
+                        url: '#',
+                        current: false,
+                    },
+                    {
+                        text: 'Videos',
+                        url: '#',
+                        current: false,
+                    },
+                    {
+                        text: 'Fans',
+                        url: '#',
+                        current: false,
+                    },
+                    {
+                        text: 'News',
+                        url: '#',
+                        current: false,
+                    },
+                    {
+                        text: 'Shop',
+                        url: '#',
+                        current: false,
+                    },
+                ]
+            }
+        }
+    }   
 </script>
 <template>
-
+    <header>
+        <nav>
+            <ul>
+                <li></li>
+            </ul>
+        </nav>
+    </header>
 </template>
 <style>
 
